ab431d99ee6ef2c0a0e7a624a59be3eab78d511e
parkMsu <mrboo77765@gmail.com>
2023-07-20 10:12:40 +0900

    프런트 단 javascript 파일 만듬

--- app/app.js
+++ app/app.js
@@ -29,7 +29,8 @@ const home = require("./src/routes/home");
 //앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
-
+                        //app.js위치 안에있는 src
+app.use(express.static(__dirname+'/src/public'));
 app.use("/", home); // use -> 미들웨어를 등록해주는 메서드
 
 module.exports = app;

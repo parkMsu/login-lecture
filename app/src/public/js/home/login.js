db9c645f5a0863a990a0e524546a1c8fd82c4599
parkMsu <mrboo77765@gmail.com>
2023-07-21 08:48:40 +0900

    서버 응답데이터 처리

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -20,5 +20,15 @@ function login(){
         body: JSON.stringify(req),
     })
     .then((res) => res.json())
-    .then(console.log);
+    .then((res)=>{
+        if(res.success){
+            location.href ="/";
+        }else{
+            // 상단에 메시지 띄움
+            alert(res.msg);
+        }
+    })
+    .catch((err)=>{
+        console.error(new Error("로그인 중 에러 발생"));
+    });
 }

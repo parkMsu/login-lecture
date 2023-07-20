06edbf0c0e86e8e1b9366b5542b802f0d30d12e6
parkMsu <mrboo77765@gmail.com>
2023-07-21 08:36:28 +0900

    유저 데이터 만들어서 인증기능

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -11,12 +11,14 @@ function login(){
         id: id.value,
         psword: psword.value,
     };
-    console.log(req, JSON.stringify(req));
-    fetch("/login",{
+    
+    fetch("/login", {
         method: "POST",
         headers:{
             "Content-Type": "application/json",
         },
-        body:JSON.stringify(req),
+        body: JSON.stringify(req),
     })
-};
+    .then((res) => res.json())
+    .then(console.log);
+}
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,45 @@
 "use strict";
 
-const home = (req, res)=>{
-    res.render("home/index");
+
+const output={
+     home: (req, res)=>{
+        res.render("home/index");
+    },    
+     login:  (req, res)=> {
+        res.render("home/login");
+     },
 };
 
-const login =  (req, res)=> {
-    res.render("home/login");
- };
+const users = {
+    id : ["mrboo777", "박형제", "박열정"],
+    psword : ["1234", "12345", "alstn718"],
+};
+
+const process = {
+    login: (req, res) =>{
+        const id = req.body.id,
+            psword = req.body.psword;
+        
+
+        if(users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx] === psword){
+                return res.json({
+                    success:true,
+                });
+            }
+        }
+
+
+        return res.json({
+            success:false,
+            msg: "로그인에 실패",
+        });
+    },
+};
+
 
  module.exports = {
-    home,
-    login,
+    output,
+    process,
  };

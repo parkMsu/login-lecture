e2cc298f84cacf40cf480b269272e804a86c8050
parkMsu <mrboo77765@gmail.com>
2023-07-24 20:02:45 +0900

    User 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -7,19 +7,31 @@ class UserStorage{
     id : ["mrboo777", "박형제", "박열정"],
     psword : ["1234", "12345", "alstn718"],
     name: ["나일진", "나이진", "나삼진"],
-};
+    };
 
-static getUsers(...fields){
-    const users = this.#users;
-    const newUsers = fields.reduce((newUsers, field)=>{
-        if(users.hasOwnProperty(field)){
-            newUsers[field] = users[field];
-        }
+    static getUsers(...fields){
+        const users = this.#users;
+        const newUsers = fields.reduce((newUsers, field)=>{
+            if(users.hasOwnProperty(field)){
+                newUsers[field] = users[field];
+            }
+            return newUsers;
+
+        }, {});
         return newUsers;
+    }
 
-    }, {});
-    return newUsers;
-}
+    static getUserInfo(id){
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const userKeys = Object.keys(users); // => [id, psword, name]
+        const userInfo = userKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+
+        return userInfo;
+    }
 }
 
 module.exports = UserStorage;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output={
      home: (req, res)=>{
@@ -14,24 +14,12 @@ const output={
 
 const process = {
     login: (req, res) =>{
-        const id = req.body.id,
-            psword = req.body.psword;
-        
 
-            const users = UserStorage.getUsers("id","psword");
-
-        const response = {};
-        if(users.id.includes(id)){
-            const idx = users.id.indexOf(id);
-            if(users.psword[idx] === psword){
-                response.success = true;
-                return res.json(response);
-            }
-        }
-
-        response.success = false;
-        response.msg = "로그인에 실패";
+        const user = new User(req.body);
+        const response = user.login();
+        console.log(response);
         return res.json(response);
+        
     },
 };
 

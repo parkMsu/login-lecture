db70835552f05247df4b9c880db1c03b3788bacb
parkMsu <mrboo77765@gmail.com>
2023-07-27 09:46:40 +0900

    fs모듈을 통한 회원가입 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -16,8 +16,10 @@ class UserStorage{
         return userInfo;
     }
 
-    static getUsers(...fields){
-        // const users = this.#users;
+    static #getUsers(data,isAll, fields){
+        // data는 버퍼데이터여서 parse 해준다
+        const users = JSON.parse(data);
+        if(isAll) return users;
         const newUsers = fields.reduce((newUsers, field)=>{
             if(users.hasOwnProperty(field)){
                 newUsers[field] = users[field];
@@ -26,6 +28,15 @@ class UserStorage{
 
         }, {});
         return newUsers;
+    }
+    static getUsers(isAll, ...fields){
+        return fs
+         .readFile("./src/databases/users.json")
+         .then((data) =>{
+            return this.#getUsers(data,isAll, fields);
+        })
+        .catch(console.error);
+        
     }
 
     static getUserInfo(id){
@@ -38,12 +49,17 @@ class UserStorage{
       
     }
    
-    static save(userInfo){
-        // const users= this.#users;
+    static async save(userInfo){
+        const users = await this.getUsers(true);
+        if(users.id.includes(userInfo.id)){
+           throw "이미 존재하는 아이디 입니다.";
+        }
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.psword.push(userInfo.psword);
-        return{ success: true};
+        //데이터 추가                               //문자열 형식으로 변경
+        fs.writeFile("./src/databases/users.json", JSON.stringify(users));
+        return {success: true};
     }
 }
 

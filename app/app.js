23b6a3bb90a1415f1274d61896c3527fd561f094
parkMsu <mrboo77765@gmail.com>
2023-08-03 07:37:41 +0900

    로그 관리 모듈 winston 등록 및 설정

--- app/app.js
+++ app/app.js
@@ -24,12 +24,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");   //어떤 OS에서 개발하더라도 동일하게 환경변수 등록 가능
-dotenv.config();
+// const morgan = require("morgan");
+
+
 
 const app =express();
+dotenv.config();        //env에 자동적으로 접근할 수 있도록 등록해줌
+
+// const accessLogStream = require("./src/config/logger");
 
 //라우팅
 const home = require("./src/routes/home");
+
 //앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
@@ -38,6 +44,8 @@ app.use(express.static(__dirname+'/src/public'));
 app.use(bodyParser.json());
 //URL을 통해 전달되는 데이터에 한글, 공백들과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(morgan("dev"));
+// app.use(morgan("common", {stream: accessLogStream }));
 app.use("/", home); // use -> 미들웨어를 등록해주는 메서드
 
 module.exports = app;
--- app/src/config/db.js
+++ app/src/config/db.js
@@ -1,3 +1,5 @@
+
+
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
